3dbdaab1b8d0b879d4411281eb20ecc7a8658775
Rajat Das <rajat@Rajats-MacBook-Air.local>
2023-08-14 18:49:32 +0530

    added page content, and footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,6 @@
+import { absoluteUrl } from "@/lib/utils";
+import { allPages } from "contentlayer/generated";
+import { compareDesc } from "date-fns";
 import Link from "next/link";
 import React from "react";
 
@@ -5,9 +8,11 @@ const Footer = () => {
   return (
     <footer className="text-muted-foreground space-y-4 p-default shrink-0">
       <ul className="flex items-center space-x-4 justify-center">
-        <li>Privacy</li>
-        <li>Terms</li>
-        <li>Disclosure</li>
+        {allPages.map((page) => (
+          <li key={page._id}>
+            <Link href={absoluteUrl(page.slugAsParams)}>{page.title}</Link>
+          </li>
+        ))}
       </ul>
       <p className="text-center text-sm">
         © 2023{" "}
