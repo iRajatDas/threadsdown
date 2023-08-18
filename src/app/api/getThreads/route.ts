36f0ace16fcdfe3e1fa2b8077b34db3160b5371f
Rajat Das <rajat@Rajats-MacBook-Air.local>
2023-08-18 13:28:43 +0530

    api types

--- src/app/api/getThreads/route.ts
+++ src/app/api/getThreads/route.ts
@@ -1,9 +1,32 @@
-import { ThreadsAPI } from "threads-api";
+import { Caption, Post, Thread, ThreadsAPI, ThreadsUser } from "threads-api";
+
+enum ProcessedResultType {
+  SingleVideo = "Single Video",
+  SinglePhoto = "Single Photo",
+  MultiplePhotos = "Multiple Photos",
+  MultipleVideos = "Multiple Videos",
+  PhotoAndVideo = "Photo(s) and Video(s) (Mixed)",
+  Unknown = "Unknown",
+}
+
+interface Media {
+  type: "image" | "video" | "images" | "videos";
+  url: string;
+  height: number;
+  width: number;
+}
+
+interface ProcessedResult {
+  type: ProcessedResultType;
+  caption?: Caption | null | undefined;
+  user?: ThreadsUser;
+  media?: Media[];
+}
 
 const threadsAPI = new ThreadsAPI({
   deviceID: "android-2vhi2rsxehy00000",
-  // username: "rajatdas.me",
-  // password: "rajatdas@123#S$"
+  username: "rajatdas.me",
+  password: "rajatdas@123#S$",
 });
 
 async function getPostId(ThreadId: string) {
@@ -48,60 +71,144 @@ export async function GET(request: Request) {
   }
 
   const data = await getPostDetailsWithoutId(url);
-  console.log("🚀 ~ file: route.ts:47 ~ GET ~ data:", typeof data, data);
-
   if (!data) {
     return new Response("Something went wrong with the request.", {
       status: 404,
     });
   }
 
-  const media = data?.containing_thread.thread_items.map((thread) => {
-    let mediaItem = thread.post;
-    mediaItem =
-      mediaItem.text_post_app_info.share_info.quoted_post ||
-      mediaItem.text_post_app_info.share_info.reposted_post ||
-      mediaItem;
+  function processApiResponse(apiResponse: {
+    containing_thread: Thread;
+    reply_threads?: Thread[] | undefined;
+  }): ProcessedResult {
+    const threadItems = apiResponse.containing_thread.thread_items;
 
-    if (mediaItem.carousel_media) {
-      const carouselMedia = mediaItem.carousel_media;
-      const videos = carouselMedia.filter((item) => item.video_versions);
-      const photos = carouselMedia.filter((item) => !item.video_versions);
+    let hasSinglePhoto = false;
+    let hasSingleVideo = false;
+    let hasMultipleVideo = false;
+    let hasMultiplePhoto = false;
 
-      if (videos.length > 0) {
-        return {
-          user: mediaItem.user,
-          type: "videos",
-          media: videos.map((item) => item.video_versions[0]),
-          photos: videos.map(
-            (item) => item.image_versions2.candidates[0] || []
-          ),
-          width: mediaItem.original_width,
-          height: mediaItem.original_height,
-        };
-      } else if (photos.length > 0) {
-        return {
-          user: mediaItem.user,
-          type: "photos",
-          media: photos.map((item) => item.image_versions2.candidates[0]),
-          width: mediaItem.original_width,
-          height: mediaItem.original_height,
-        };
+    for (const threadItem of threadItems) {
+      const post = threadItem.post;
+
+      let hasMultipleVideoVersions = false;
+
+      if (post.carousel_media) {
+        for (const item of post?.carousel_media) {
+          if (item.video_versions.length > 1) {
+            hasMultipleVideoVersions = true;
+            break; // Exit the loop as soon as a match is found
+          }
+        }
+      }
+
+      // Check if single photo
+      if (
+        post.image_versions2 &&
+        post.image_versions2.candidates.length > 0 &&
+        post.video_versions.length === 0 &&
+        post.carousel_media_count === null
+      ) {
+        hasSinglePhoto = true;
+      }
+
+      // Check if single video
+      if (
+        post.video_versions &&
+        post.video_versions.length > 0 &&
+        post.carousel_media_count === null
+      ) {
+        hasSingleVideo = true;
+      }
+
+      // Check if mutliple photo
+      if (
+        post.video_versions &&
+        post.video_versions.length === 0 &&
+        post.carousel_media_count >= 0 &&
+        !hasMultipleVideoVersions
+      ) {
+        hasMultiplePhoto = true;
+      }
+
+      // Check if mutliple video
+      if (
+        post.video_versions &&
+        post.video_versions.length === 0 &&
+        post.carousel_media_count >= 1 &&
+        hasMultipleVideoVersions &&
+        !hasMultiplePhoto
+      ) {
+        hasMultipleVideo = true;
       }
     }
 
-    if (mediaItem.video_versions && mediaItem.video_versions.length > 0) {
-      return {
-        user: mediaItem.user,
-        type: "videos",
-        media: [...mediaItem.video_versions],
-        width: mediaItem.original_width,
-        height: mediaItem.original_height,
-      };
-    }
-  });
+    const post = threadItems[0].post;
 
-  return new Response(JSON.stringify(media), {
+    if (hasSinglePhoto)
+      return {
+        type: ProcessedResultType.SinglePhoto,
+        user: post.user,
+        caption: post.caption,
+        media: [
+          {
+            type: "image",
+            height: post.image_versions2.candidates[0].height,
+            width: post.image_versions2.candidates[0].width,
+            url: post.image_versions2.candidates[0].url,
+          },
+        ],
+      };
+    else if (hasMultiplePhoto) {
+      return {
+        type: ProcessedResultType.MultiplePhotos,
+        user: post.user,
+        caption: post.caption,
+        media: post.carousel_media.map((photo) => {
+          // console.log(photo.image_versions2);
+          return {
+            type: "images",
+            height: photo.original_height || 0,
+            width: photo.original_width || 0,
+            url: photo.image_versions2.candidates[0]?.url || "",
+          };
+        }),
+      };
+    } else if (hasSingleVideo)
+      return {
+        type: ProcessedResultType.SingleVideo,
+        user: post.user,
+        caption: post.caption,
+        media: [
+          {
+            type: "video",
+            height: post.video_versions[0].original_height || 0,
+            width: post.video_versions[0].original_width || 0,
+            url: post.video_versions[0]?.url || "",
+          },
+        ],
+      };
+    else if (hasMultipleVideo)
+      return {
+        type: ProcessedResultType.MultipleVideos,
+        user: post.user,
+        caption: post.caption,
+        media: post.carousel_media.map((video) => {
+          console.log(video);
+          return {
+            type: "videos",
+            height: video.original_height || 0,
+            width: video.original_width || 0,
+            url: video.video_versions[0]?.url || "",
+          };
+        }),
+      };
+    else return { type: ProcessedResultType.PhotoAndVideo };
+  }
+
+  const processedResult = processApiResponse(data);
+
+  return new Response(JSON.stringify({ processedResult, data }), {
     status: 200,
     headers: {
       "Access-Control-Allow-Origin": origin,
